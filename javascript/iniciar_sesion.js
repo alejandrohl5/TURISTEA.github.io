9cdb764b1d3de984684fb20fe5379e6e2bf6decf
alejandrohl5 <81606955+alejandrohl5@users.noreply.github.com>
2021-10-19 00:56:37 -0500

    login

---
+++ javascript/iniciar_sesion.js
@@ -0,0 +1,23 @@
+const guardar = document.querySelector('#formulario_login');
+
+guardar.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const correo = document.querySelector('#correo').value;
+    const contrasena = document.querySelector('#contrasena').value;
+
+  
+    alert("BIENVENIDO!!!")
+    guardar.reset();
+
+    auth
+          .createUserWithEmailAndPassword(correo, contrasena)   
+
+          .then(userCredential => {
+
+              console.log('hecho con exito');
+
+          })
+
+          window.location.replace("http://127.0.0.1:5501/index.html");
+   })
+
